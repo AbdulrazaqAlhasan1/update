f352ba38670b38c29f62c984f3b5f9f91f82bdd0
unknown <you@example.com>
2019-01-25 14:42:29 +0100

    hasse har fixat ordning updates måste göras

---
+++ js/dashboard.js
@@ -0,0 +1,676 @@
+(function($) {
+  'use strict';
+  $(function() {
+
+    if ($("#total-sales-chart").length) {
+
+        /*total sales charts*/
+        let totalsales = new XMLHttpRequest()
+        totalsales.onload = function(){
+            if(this.readyState == 4 && this.status == 200){
+                var object1 = JSON.parse(this.response);
+     // console.log( object1.datasets[0].data);
+      //console.log( object1.datasets[1].label);
+    
+  
+
+      var areaData = {
+        labels: object1.labels,
+        datasets: [
+          {
+           data: object1.datasets[0].data,
+            backgroundColor: [
+              'rgba(61, 165, 244, .0)'
+            ],
+            borderColor: [
+              'rgb(61, 165, 244)'
+            ],
+            borderWidth: 2,
+            fill: 'origin',
+            label: object1.datasets[0].label,
+          },
+          {
+           
+            data: object1.datasets[1].data,
+            backgroundColor: [
+              'rgba(241, 83, 110, .0)'
+            ],
+            borderColor: [
+              'rgb(241, 83, 110)'
+            ],
+            borderWidth: 2,
+            fill: 'origin',
+            label: object1.datasets[1].label,
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              padding: 20,
+              fontColor:"#000",
+              fontSize: 14
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 100,
+              fontColor: "#000",
+              fontSize: 14,
+              padding: 18,
+              stepSize: 100000,
+              callback: function(value) {
+                var ranges = [
+                    { divider: 1e6, suffix: 'M' },
+                    { divider: 1e3, suffix: 'k' }
+                ];
+                function formatNumber(n) {
+                    for (var i = 0; i < ranges.length; i++) {
+                      if (n >= ranges[i].divider) {
+                          return (n / ranges[i].divider).toString() + ranges[i].suffix;
+                      }
+                    }
+                    return n;
+                }
+                return formatNumber(value);
+              }
+            },
+            gridLines: {
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .37
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var revenueChartCanvas = $("#total-sales-chart").get(0).getContext("2d");
+      var revenueChart = new Chart(revenueChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+    }
+    totalsales.open("GET" , "https://fe18.azurewebsites.net/api/totalsaleschart ", true);
+    totalsales.send();
+  }
+ 
+
+    if ($("#users-chart").length) {
+      let usercharts = new XMLHttpRequest()
+    usercharts.onload = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var object2 = JSON.parse(this.response);
+       // console.log(object2);
+       // console.log(object2.datasets[0].data);
+        var areaData = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
+        datasets: [{
+         
+            data: object2.datasets[0].data,
+            backgroundColor: [
+              '#e0fff4'
+            ],
+            borderWidth: 2,
+            borderColor: "#00c689",
+            fill: 'origin',
+            label: object2.datasets[0].label,
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+            ticks: {
+              display: true
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: false,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 100,
+              min: 0,
+              max: 300
+            },
+            gridLines: {
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .35
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var salesChartCanvas = $("#users-chart").get(0).getContext("2d");
+      var salesChart = new Chart(salesChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+  }
+  usercharts.open("GET" , "https://fe18.azurewebsites.net/api/userschart ", true);
+  usercharts.send();
+    }
+
+    if ($("#projects-chart").length) {
+      let projectscharts = new XMLHttpRequest()
+      projectscharts.onload = function(){
+      if(this.readyState == 4 && this.status == 200){
+          var object3 = JSON.parse(this.response);
+         // console.log(object3);
+      var areaData = {
+        labels: object3.labels,
+        datasets: [{
+            data: object3.datasets[0].data,
+            backgroundColor: [
+              '#e5f2ff'
+            ],
+            borderWidth: 2,
+            borderColor: "#3da5f4",
+            fill: 'origin',
+            label: object3.datasets[0].label,
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          filler: {
+            propagate: false
+          }
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+            ticks: {
+              display: true
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+              color: 'transparent',
+              zeroLineColor: '#eeeeee'
+            }
+          }],
+          yAxes: [{
+            display: false,
+            ticks: {
+              display: true,
+              autoSkip: false,
+              maxRotation: 0,
+              stepSize: 100,
+              min: 0,
+              max: 300
+            },
+            gridLines: {
+              drawBorder: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        elements: {
+          line: {
+            tension: .05
+          },
+          point: {
+            radius: 0
+          }
+        }
+      }
+      var salesChartCanvas = $("#projects-chart").get(0).getContext("2d");
+      var salesChart = new Chart(salesChartCanvas, {
+        type: 'line',
+        data: areaData,
+        options: areaOptions
+      });
+    }
+  }
+  projectscharts.open("GET" , "https://fe18.azurewebsites.net/api/projectschart ", true);
+  projectscharts.send();
+    }
+
+    if ($('#offlineProgress').length) {
+      var bar = new ProgressBar.Circle(offlineProgress, {
+        color: '#000',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 6,
+        trailWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        text: {
+          autoStyleContainer: true,
+          style : {
+            color : "#fff",
+            position: 'absolute',
+            left: '40%',
+            top: '50%'
+          }
+        },
+        svgStyle: {
+          width: '90%'
+        },
+        from: {
+          color: '#f1536e',
+          width: 6
+        },
+        to: {
+          color: '#f1536e',
+          width: 6
+        },
+        // Set default step function for all animate calls
+        step: function(state, circle) {
+          circle.path.setAttribute('stroke', state.color);
+          circle.path.setAttribute('stroke-width', state.width);
+  
+          var value = Math.round(circle.value() * 100);
+          if (value === 0) {
+            circle.setText('');
+          } else {
+            circle.setText(value);
+          }
+  
+        }
+      });
+  
+      bar.text.style.fontSize = '1rem';
+      bar.animate(.64); // Number from 0.0 to 1.0
+    }
+
+    if ($('#onlineProgress').length) {
+      var bar = new ProgressBar.Circle(onlineProgress, {
+        color: '#000',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 6,
+        trailWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        text: {
+          autoStyleContainer: true,
+          style : {
+            color : "#fff",
+            position: 'absolute',
+            left: '40%',
+            top: '50%'
+          }
+        },
+        svgStyle: {
+          width: '90%'
+        },
+        from: {
+          color: '#fda006',
+          width: 6
+        },
+        to: {
+          color: '#fda006',
+          width: 6
+        },
+        // Set default step function for all animate calls
+        step: function(state, circle) {
+          circle.path.setAttribute('stroke', state.color);
+          circle.path.setAttribute('stroke-width', state.width);
+  
+          var value = Math.round(circle.value() * 100);
+          if (value === 0) {
+            circle.setText('');
+          } else {
+            circle.setText(value);
+          }
+  
+        }
+      });
+  
+      bar.text.style.fontSize = '1rem';
+      bar.animate(.84); // Number from 0.0 to 1.0
+    }
+
+    if ($("#revenue-chart").length) {
+      let revenuchart = new XMLHttpRequest()
+      revenuchart .onload = function(){
+     if(this.readyState == 4 && this.status == 200){
+        var object5 = JSON.parse(this.response);
+       // console.log(object5);
+       var CurrentChartCanvas = $("#revenue-chart").get(0).getContext("2d");
+       var CurrentChart = new Chart(CurrentChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: object5.labels,
+          datasets: [{
+              label: object5.datasets[0].label,
+              data: object5.datasets[0].data,
+              backgroundColor: '#405189'
+            },
+            {
+              label: object5.datasets[1].label,
+              data: object5.datasets[1].data,
+              backgroundColor: '#3da5f4'
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false
+              },
+              ticks: {
+                fontColor: "#000",
+                display: true,
+                fontStyle: 400,
+                fontSize: 14,
+                stepSize: 100000,
+                callback: function(value) {
+                  var ranges = [
+                      { divider: 1e6, suffix: 'M' },
+                      { divider: 1e3, suffix: 'k' }
+                  ];
+                  function formatNumber(n) {
+                      for (var i = 0; i < ranges.length; i++) {
+                        if (n >= ranges[i].divider) {
+                            return (n / ranges[i].divider).toString() + ranges[i].suffix;
+                        }
+                      }
+                      return n;
+                  }
+                  return formatNumber(value);
+                }
+              }
+            }],
+            xAxes: [{
+              stacked: false,
+              categoryPercentage: .5,
+              barPercentage: 1,
+              ticks: {
+                beginAtZero: true,
+                fontColor: "#000",
+                display: true,
+                fontSize: 14
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: true
+              },
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+  }
+  
+  revenuchart.open("GET" , "https://fe18.azurewebsites.net/api/revenuechart" , true);
+  revenuchart.send();
+   
+ }
+
+    if ($("#distribution-chart").length) {
+      let distribution = new XMLHttpRequest()
+      distribution .onload = function(){
+     if(this.readyState == 4 && this.status == 200){
+        var object6 = JSON.parse(this.response);
+       console.log(object6.datasets[0].city[0]);
+      var areaData = {
+        labels: object6.labels,
+        datasets: [{
+            data: object6.datasets[0].data,
+            backgroundColor: [
+              "#3da5f4", "#f1536e", "#fda006"
+            ],
+            borderColor: "rgba(0,0,0,0)"
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        segmentShowStroke: false,
+        cutoutPercentage: 72,
+        elements: {
+          arc: {
+              borderWidth: 4
+          }
+        },      
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        },
+        legendCallback: function(chart) { 
+          console.log(object6.datasets[0].city[0]);
+          var text = [];
+          text.push('<div class="distribution-chart">');
+            text.push('<div class="item"><div class="legend-label" style="border: 3px solid ' + chart.data.datasets[0].backgroundColor[0] + '"></div>');
+            text.push('<p >' +object6.datasets[0].city[0]+ '</p>');
+            text.push('</div>');
+            text.push('<div class="item"><div class="legend-label" style="border: 3px solid ' + chart.data.datasets[0].backgroundColor[1] + '"></div>');
+            text.push('<p >' +object6.datasets[0].city[1]+ '</p>');
+            text.push('</div>');
+            text.push('<div class="item"><div class="legend-label" style="border: 3px solid ' + chart.data.datasets[0].backgroundColor[2] + '"></div>');
+            text.push('<p> ' +object6.datasets[0].city[2]+ '</p>');
+          
+            text.push('</div>');
+          text.push('</div>');
+          return text.join("");
+      
+        },
+      }
+      var distributionChartPlugins = {
+        beforeDraw: function(chart) {
+          var width = chart.chart.width,
+              height = chart.chart.height,
+              ctx = chart.chart.ctx;
+      
+          ctx.restore();
+          var fontSize = .96;
+          ctx.font = "600 " + fontSize + "em sans-serif";
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#000";
+      
+          var text = "70%",
+              textX = Math.round((width - ctx.measureText(text).width) / 2),
+              textY = height / 2;
+      
+          ctx.fillText(text, textX, textY);
+          ctx.save();
+        }
+      }
+      var distributionChartCanvas = $("#distribution-chart").get(0).getContext("2d");
+      var distributionChart = new Chart(distributionChartCanvas, {
+        type: 'doughnut',
+        data: areaData,
+        options: areaOptions,
+        plugins: distributionChartPlugins
+      });
+      document.getElementById('distribution-legend').innerHTML = distributionChart.generateLegend();
+    }
+   }
+  
+    distribution.open("GET" , "https://fe18.azurewebsites.net/api/distributionchart" , true);
+     distribution.send();
+   
+    }
+
+    if ($("#sale-report-chart").length) {
+      let salereport = new XMLHttpRequest();
+
+      salereport.onload = function () {
+          if(this.readyState == 4 && this.status == 200) {
+                
+                 var object7 = JSON.parse(this.response);
+                 console.log(object7);
+               var CurrentChartCanvas = $("#sale-report-chart").get(0).getContext("2d");
+      
+                    var CurrentChart = new Chart(CurrentChartCanvas, {
+              type: 'bar',
+                 data: {
+          labels: object7.labels,
+          
+          datasets: [{
+              label: object7.datasets[0].label,
+              data: object7.datasets[0].data,
+              backgroundColor: ["#3da5f4","#e0f2ff","#3da5f4","#e0f2ff","#3da5f4","#e0f2ff","#3da5f4","#e0f2ff","#3da5f4","#e0f2ff","#3da5f4"]
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false
+              },
+              ticks: {
+                fontColor: "#000",
+                display: true,
+                padding: 20,
+                fontSize: 14,
+                stepSize: 10000,
+                callback: function(value) {
+                  var ranges = [
+                      { divider: 1e6, suffix: 'M' },
+                      { divider: 1e3, suffix: 'k' }
+                  ];
+                  function formatNumber(n) {
+                      for (var i = 0; i < ranges.length; i++) {
+                        if (n >= ranges[i].divider) {
+                            return (n / ranges[i].divider).toString() + ranges[i].suffix;
+                        }
+                      }
+                      return n;
+                  }
+                  return "$" + formatNumber(value);
+                }
+              }
+            }],
+            xAxes: [{
+              stacked: false,
+              categoryPercentage: .6,
+              ticks: {
+                beginAtZero: true,
+                fontColor: "#000",
+                display: true,
+                padding: 20,
+                fontSize: 14
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: true
+              },
+              barPercentage: .7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+       }
+      }
+     salereport.open("GET" , "https://fe18.azurewebsites.net/api/salereportchart ", true);
+      salereport.send();
+    }
+
+  });
+})(jQuery);
